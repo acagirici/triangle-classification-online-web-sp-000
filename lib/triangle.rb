36e8268e890c3a42feadeec3a9bcd9171fe05bba
Arif Cagirici <acagirici23@gmail.com>
2020-07-04 18:35:27 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,12 @@ class Triangle
   else
     if (@side_1 != @side_2 && @side_2 != @side_3 && @side_1 != @side_3)
         :scalene
-    elsif (@)
+    elsif (@side_1 == @side_2) && (@side_2 == @side_3)
+        :equilateral
+    elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+        :isosceles
+      end
+    end
   end
   
   class TriangleError < StandardError
