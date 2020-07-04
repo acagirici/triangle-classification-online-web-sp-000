c5663d9d7865bf029aeb57f1f50f28b0175fe850
Arif Cagirici <acagirici23@gmail.com>
2020-07-04 18:33:12 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,8 @@ class Triangle
     elsif (@side_1+@side_2 <= @side_3 || @side_1+@side_3 <= @side_2 || @side_2+@side_3 <= @side_1)
     raise TriangleError
   else
-    if (@side_1 != @side_2 && @side_2 != @side_3 
+    if (@side_1 != @side_2 && @side_2 != @side_3 && @side_1 != @side_3)
+        :
   end
   
   class TriangleError < StandardError
