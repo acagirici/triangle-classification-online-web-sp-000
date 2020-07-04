b74697eab8b3fe4f1de49cc1d9b09442062cfed1
Arif Cagirici <acagirici23@gmail.com>
2020-07-04 18:26:09 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,11 +1,13 @@
 class Triangle
-  attr_accessor :name 
-  
-  def initialize(name)
-    @name = name
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
   end
   
-  def kind
+  def kind()
+    if (@side_1 <= 0 || @side_2 <= 0 || @side_3 <= 0)
+      raise TriangleError
     
   end
   
